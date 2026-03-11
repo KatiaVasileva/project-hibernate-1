f08e362130a4fe718daa72480ec1822015e2d989
KatiaVasileva <eka.vasileva@gmail.com>
2026-03-11 12:18:54 +0500

    IMPLEMENT requests to DB

--- src/main/java/com/game/entity/Player.java
+++ src/main/java/com/game/entity/Player.java
@@ -1,23 +1,40 @@
 package com.game.entity;
 
+import jakarta.persistence.*;
+
 import java.util.Date;
 
-
+@Entity
+@Table(name = "player", schema = "rpg")
+@NamedQuery(name = Player.GET_TOTAL_NUMBER_OF_PLAYERS, query = "SELECT COUNT(p) FROM Player p")
 public class Player {
+    public static final String GET_TOTAL_NUMBER_OF_PLAYERS = "Player.GET_TOTAL_NUMBER_OF_PLAYERS";
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false, length = 12)
     private String name;
 
+    @Column(nullable = false, length = 30)
     private String title;
 
+    @Enumerated(EnumType.ORDINAL)
+    @Column(nullable = false)
     private Race race;
 
+    @Enumerated(EnumType.ORDINAL)
+    @Column(nullable = false)
     private Profession profession;
 
+    @Column(nullable = false)
     private Date birthday;
 
+    @Column(nullable = false)
     private Boolean banned;
 
+    @Column(nullable = false)
     private Integer level;
 
     public Player() {
--- src/main/java/com/game/repository/PlayerRepositoryDB.java
+++ src/main/java/com/game/repository/PlayerRepositoryDB.java
@@ -1,51 +1,135 @@
 package com.game.repository;
 
 import com.game.entity.Player;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import org.hibernate.query.Query;
 import org.springframework.stereotype.Repository;
 
 import javax.annotation.PreDestroy;
 import java.util.List;
 import java.util.Optional;
+import java.util.Properties;
+
+import static com.game.entity.Player.GET_TOTAL_NUMBER_OF_PLAYERS;
 
 @Repository(value = "db")
 public class PlayerRepositoryDB implements IPlayerRepository {
 
-    public PlayerRepositoryDB() {
+    private final SessionFactory sessionFactory;
 
+    public PlayerRepositoryDB() {
+        Properties properties = new Properties();
+        properties.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
+        properties.put(Environment.URL, "jdbc:mysql://localhost:3306/rpg");
+        properties.put("hibernate.connection.useUnicode", true);
+        properties.put(Environment.SHOW_SQL, "true");
+        properties.put(Environment.FORMAT_SQL, "true");
+        properties.put(Environment.USE_SQL_COMMENTS, "true");
+        properties.put(Environment.GENERATE_STATISTICS, "true");
+        properties.put(Environment.USER, "root");
+        properties.put(Environment.PASS, "mysql");
+        properties.put(Environment.HBM2DDL_AUTO, "update");
+
+        this.sessionFactory = new Configuration()
+                .setProperties(properties)
+                .addAnnotatedClass(Player.class)
+                .buildSessionFactory();
     }
 
     @Override
     public List<Player> getAll(int pageNumber, int pageSize) {
-        return null;
+        try (Session session = sessionFactory.openSession()) {
+            int offset = pageNumber * pageSize;
+
+            Query<Player> query = session.createNativeQuery(
+                    "SELECT * FROM rpg.player LIMIT :pageSize OFFSET :offset",
+                    Player.class);
+
+            query.setParameter("pageSize", pageSize);
+            query.setParameter("offset", offset);
+
+            return query.getResultList();
+        } catch (Exception e) {
+            System.err.println("Error when getting al players: " + e.getMessage());
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public int getAllCount() {
-        return 0;
+        try(Session session = sessionFactory.openSession()) {
+            Query<Long> query = session.createNamedQuery(GET_TOTAL_NUMBER_OF_PLAYERS, Long.class);
+            Long count = query.getSingleResult();
+            return count.intValue();
+        } catch (Exception e) {
+            System.err.println("Error when getting error count: " + e.getMessage());
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public Player save(Player player) {
-        return null;
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        try {
+            session.persist(player);
+            transaction.commit();
+            return player;
+        } catch (Exception e) {
+            transaction.rollback();
+            System.out.println("Error when saving player with id = : " + player.getId() + ": " + e.getMessage());
+            throw new RuntimeException();
+        } finally {
+            session.close();
+        }
     }
 
     @Override
     public Player update(Player player) {
-        return null;
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        try {
+            session.merge(player);
+            transaction.commit();
+            return player;
+        } catch (Exception e) {
+            transaction.rollback();
+            System.out.println("Error when updating player with id = : " + player.getId() + ": " + e.getMessage());
+            throw new RuntimeException();
+        }
     }
 
     @Override
     public Optional<Player> findById(long id) {
-        return Optional.empty();
+        try (Session session = sessionFactory.openSession()) {
+            Query<Player> query = session.createQuery("SELECT p FROM Player p WHERE id = :id", Player.class);
+            query.setParameter("id", id);
+            return Optional.ofNullable(query.getSingleResult());
+        } catch (Exception e) {
+            System.err.println("Error when finding player with id : " + id + ": " + e.getMessage());
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public void delete(Player player) {
-
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        try {
+            session.delete(player);
+            transaction.commit();
+        } catch (Exception e) {
+            transaction.rollback();
+            System.out.println("Error when deleting player with id = : " + player.getId() + ": " + e.getMessage());
+        }
     }
 
     @PreDestroy
     public void beforeStop() {
-
+        sessionFactory.close();
     }
 }
